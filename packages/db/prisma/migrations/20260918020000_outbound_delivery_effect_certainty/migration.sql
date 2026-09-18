c61f16ad3eabf155927403af4afcbcbcb670e887
Sachin Dookie <sachdookie.pro@gmail.com>
2026-09-17 21:50:57 -0400

    feat(communications): separate Resend provider and consequence phases

--- packages/db/prisma/migrations/20260918020000_outbound_delivery_effect_certainty/migration.sql
+++ packages/db/prisma/migrations/20260918020000_outbound_delivery_effect_certainty/migration.sql
@@ -23,3 +23,6 @@ CREATE INDEX "outbound_deliveries_business_id_consequence_state_idx"
 
 CREATE INDEX "delivery_events_delivery_id_attempt_id_idx"
   ON "delivery_events"("delivery_id", "attempt_id");
+
+CREATE UNIQUE INDEX "delivery_events_delivery_id_event_type_attempt_id_key"
+  ON "delivery_events"("delivery_id", "event_type", "attempt_id");
